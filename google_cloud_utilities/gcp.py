71961617a5e35416c1e155e4fbc34eceb4ca8fca
John Foley <johnf1004@yahoo.co.uk>
2024-06-14 12:30:11 +0100

    Add expiration option to create_bq_table

--- google_cloud_utilities/gcp.py
+++ google_cloud_utilities/gcp.py
@@ -91,7 +91,7 @@ def execute_bigquery_command(sql, bq_client, location='EU'):
     return result
 
 
-def create_bq_table(table_id, partition_col, partition_type, schema, bq_client):
+def create_bq_table(table_id, partition_col, partition_type, schema, bq_client, expires=None):
     """
     Create partitioned table in bigquery
     :param table_id: Name of table to be created; projectname.datasetname.table
@@ -99,6 +99,7 @@ def create_bq_table(table_id, partition_col, partition_type, schema, bq_client):
     :param partition_type: bigquery.TimePartitioningType.MONTH or bigquery.TimePartitioningType.DAY,
     :param schema: Schema, list of bigquery.schema.SchemaField objects
     :param bq_client Bigquery client object
+    :param expires: Expiration date for the table
     """
 
     table = bigquery.Table(table_id, schema=schema)
@@ -106,6 +107,9 @@ def create_bq_table(table_id, partition_col, partition_type, schema, bq_client):
         type_=partition_type,
         field=partition_col
     )
+    if expires:
+        assert isinstance(expires, datetime), "Expiration date must be a datetime object"
+        table.expires = expires
 
     table = bq_client.create_table(table)
     logger.info(
